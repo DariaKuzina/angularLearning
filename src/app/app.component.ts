d055e20b88bb52f3dcada19cd9631be790b0643a
Daria Kuzina <daria_kuzina@epam.com>
2017-07-27 17:28:52 +0300

    task start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  subtitle = 'subtitle';
+  magicDigit : number = 7;
+  magicNumbers : Array<number> = [1,2,3];
 }
